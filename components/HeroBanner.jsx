dd09fd8d724f8f4e9cf1f8d498175c141d8b6c35
saniabdullah <abdullah.s@students.amikom.ac.id>
2022-07-26 09:18:38 +0800

    perbaikan pada font

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -6,13 +6,15 @@ const HeroBanner = ({ heroBanner }) => {
   return (
     <div className='hero-banner-container'>
       <div>
-        <h1>KOPI <span>NALAR</span></h1>
-        <h3>{ heroBanner.midText }</h3>
+        <h1>KOPI NALAR</h1>
+        <h3>Best Coffee <span>in</span> Town</h3>
 
         <div>
-          <button type='button'>
-            { heroBanner.buttonText }
-          </button>
+          <Link href={'#products'}>
+            <button type='button'>
+              { heroBanner.buttonText }
+            </button>
+          </Link>
           <div className='desc'>
             <h5>Open</h5>
             <p>{ heroBanner.desc }</p>
